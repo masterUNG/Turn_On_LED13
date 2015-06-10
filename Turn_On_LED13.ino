f4f34e5bec4e5c810dce47ff9eee7dfdfda96882
masterUNG <phrombutr@gmail.com>
2015-06-11 02:44:33 +0700

    Update

--- Turn_On_LED13.ino
+++ Turn_On_LED13.ino
@@ -1,3 +1,14 @@
-void setup{}  //initiallize digital pin output
+void setup(){
 
-void loop{}  // loop
+  pinMode(13, OUTPUT);  //Pin13 output
+
+}  //initiallize digital pin output
+
+void loop(){
+
+  digitalWrite(13, HIGH);  //Set up LED ON
+  delay(1000);             //Wait One Second
+  digitalWrite(13, LOW);   //Set up LED OFF
+  delay(1000);             //Wait One Second
+
+}  // loop
